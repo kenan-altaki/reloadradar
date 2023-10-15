cef2cb85e40eeca0a5279ac8d06fa238cb9c7955
Kenan <kenan.altaki@gmail.com>
2023-10-15 11:19:57 +0200

    initial commit

---
+++ reloadradar/reloadradar/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for reloadradar project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+# Standard Libraries
+import os
+
+# Django Libraries
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reloadradar.settings")
+
+application = get_asgi_application()
